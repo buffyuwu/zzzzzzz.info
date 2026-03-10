b02b9b463626430776f5f0ddc41814d408213ccd
buffyuwu <buffyvrc@gmail.com>
2026-03-10 14:55:02 -0400

    Update gif.js

--- js/gif.js
+++ js/gif.js
@@ -34,7 +34,7 @@ var spawnedImages = 0;
 var totalSpawned = 0;
 var spawnedText = 0;
 var MAX_TEXT = 100;
-var MAX_IMAGES = 1000;
+var MAX_IMAGES = 250;
 var videoSpawned = false;
 
 function shuffle(arr) {
